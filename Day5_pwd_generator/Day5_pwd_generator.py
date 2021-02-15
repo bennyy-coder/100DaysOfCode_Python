cb32335de39071622a2e948b1fd2b2f41adcc333
bennyy2021 <78919232+bennyy2021@users.noreply.github.com>
2021-02-16 03:08:25 +0800

    Day5 project
    password generator

---
+++ Day5_pwd_generator/Day5_pwd_generator.py
@@ -0,0 +1,33 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+pwd = ""
+while nr_letters + nr_symbols + nr_numbers > 0:
+  seed = random.randint(0, 2)
+  if seed == 0 and nr_letters > 0:
+    pwd += random.choice(letters)
+    nr_letters -= 1
+  elif seed == 1 and nr_symbols > 0:
+    pwd += random.choice(symbols)
+    nr_symbols -= 1
+  elif seed == 2 and nr_numbers > 0:
+    pwd += random.choice(numbers)
+    nr_numbers -=1
+print(pwd)
+
+
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
